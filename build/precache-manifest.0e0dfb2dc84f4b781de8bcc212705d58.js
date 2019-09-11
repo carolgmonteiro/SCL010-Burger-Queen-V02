b509a996479b1f1e29278e4cd0870730d688ca87
Adriana Sanchez <adrievelyn@gmail.com>
2019-09-10 21:59:17 -0300

    agregando instalaciones de react y firebase

---
+++ build/precache-manifest.0e0dfb2dc84f4b781de8bcc212705d58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4625c59ba68c96729968d77cc73b5c71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a48bb2ad8f18ed6216e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a14bb3b717fc80d5c733",
+    "url": "/static/js/2.bb976b1a.chunk.js"
+  },
+  {
+    "revision": "2a48bb2ad8f18ed6216e",
+    "url": "/static/js/main.34888bac.chunk.js"
+  },
+  {
+    "revision": "3ddb9bab2aec196cf5cb",
+    "url": "/static/js/runtime~main.76fa5e7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
