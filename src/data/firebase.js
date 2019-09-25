78507f0ba488819ce23e94966db3e52e52059111
carolgmonteiro <carolgmonteiro@live.com>
2019-09-25 16:47:51 -0300

    muestrando la data en firebase

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,1 +1,38 @@
 //codigo que conecta a firebase
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+const config = {
+  apiKey: "AIzaSyDn4cgn-arq6lkiAYOviSeC3IJmU8WNhFk",
+  authDomain: "burger-queen-4a800.firebaseapp.com",
+  databaseURL: "https://burger-queen-4a800.firebaseio.com",
+  projectId: "burger-queen-4a800",
+  storageBucket: "burger-queen-4a800.appspot.com",
+  messagingSenderId: "300644811053",
+  appId: "1:300644811053:web:6a21982f42529972a913a4"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+// //codigo que conecta a firebase
+// import firebase from "firebase";
+// import "firebase/firestore";
+
+// const settings = { timestampsInSnapshots: true };
+
+// // Your web app's Firebase configuration
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: "AIzaSyDn4cgn-arq6lkiAYOviSeC3IJmU8WNhFk",
+//   authDomain: "burger-queen-4a800.firebaseapp.com",
+//   databaseURL: "https://burger-queen-4a800.firebaseio.com",
+//   projectId: "burger-queen-4a800",
+//   storageBucket: "burger-queen-4a800.appspot.com",
+//   messagingSenderId: "300644811053",
+//   appId: "1:300644811053:web:6a21982f42529972a913a4"
+// });
+
+// const db = firebaseApp.firestore().settings(settings);
+
+// export { db };
