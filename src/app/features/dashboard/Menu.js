00d6c6dee2e1c3ead09fb63577d041511720f7c4
Adriana Sanchez <adrievelyn@gmail.com>
2019-09-25 07:43:01 -0300

    modificaciones en menu

--- src/app/features/dashboard/Menu.js
+++ src/app/features/dashboard/Menu.js
@@ -1,17 +1,39 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-
-import ResumeBill from "../events/ResumeBill";
 import NavBarMenu from "../nav/NavBarMenu";
+import ResumeBill from "../events/ResumeBill";
+import App from "../../../app/layout/App.css"
 
-function Menu() {
+
+
+/*function Menu() {
+    return (
+      <div className= "container">
+        <div className="row">
+          <div className="col-6">
+            <ResumeBill
+              input type="text" name="waiter" placeholder="Waiter" />
+            </div>
+          </div>
+       </div>
+
+      );
+
+};
+export default Menu;*/
+
+
+/*function Menu() {
   return (
     <div className="container">
       <div classname="row">
         <div className="col-6">
           <NavBarMenu></NavBarMenu>
         </div>
+
         <div className="col-6">
+        <ResumeBill
+              input type="text" name="waiter" placeholder="Waiter" />
           <ResumeBill>Resume</ResumeBill>
         </div>
       </div>
@@ -19,53 +41,38 @@ function Menu() {
   );
 }
 
+
+export default Menu;*/
+
+
+function Menu() {
+  return (
+     <div className="container">
+         <div classname="row">
+        <div className="col-3">
+          <NavBarMenu
+           input className="col-6" type="text" name="waiter" placeholder="Waiter" />
+
+        </div>
+
+              <div className="col-3">
+     
+              <ResumeBill/>
+
+       
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
 export default Menu;
 
-// import React from "react";
-// import "bootstrap/dist/css/bootstrap.css";
-// // import NavBarGeneral from "./features/nav/NavBarGeneral";
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import Drinks from "../menu/Drinks";
-// import Food from "../menu/Food";
-// import ResumeBill from "../events/ResumeBill";
 
-// function Menu() {
-//   return (
-//     <Router>
-//       <div className="lead">
-//         <form className="form-group">
-//           <input type="text" name="waiter" placeholder="Waiter" />
-//         </form>
-//         <ul className="navbar navbar-dark bg-dark">
-//           <a href="/drinks">
-//             <Link to="/drinks" className="text-white">
-//               Drinks
-//             </Link>
-//           </a>
-//           <a href="/main">
-//             <Link to="/main" className="text-white">
-//               Food
-//             </Link>
-//           </a>
-//         </ul>
-//         <Route exact path="/drinks" component={Drinks} />
-//         <Route path="/main" component={Food} />
 
-//         <div className="App">
-//           <div className="container">
-//             <div className="row mt-4">
-//               <div className="col-md-4 text-center">
-//                 <ResumeBill></ResumeBill>
-//               </div>
-//               <div className="col-md-8">
-//                 <div className="row"></div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// }
 
-// export default Menu;
+
--- src/app/features/events/ResumeBill.js
+++ src/app/features/events/ResumeBill.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class ResumeBill extends React.Component {
+  handleChange = (e) => {
+    console.log({
+      name: e.target.name,
+      value: e.target.value});
+  }
+
+  handleClick = (e) => {
+    console.log('Button was clicked');
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form was submitted');
+  };
+
+//el handleClick y el handleSubmit se llama enlazar eventos conectando la accion del usuario c on los componentes del React
+//se ve la accion que esta realizando el usuario tan pronto escribe
+
   render() {
     return (
       <div className="lead">
-        <form className="form-group">
-          <input type="text" name="client" placeholder="Client" />
-        </form>
-        <div className="form-group">Pedidos</div>
-      </div>
+        <form onSubmit={this.handleSubmit}
+        className="form-group">
+          <input onChange={this.handleChange} type="text" name="client" placeholder="Client" />
+      
+      <ul className="navbar navbar-dark bg-dark">
+       <a href="/pedidos">
+            <Link to="/pedidos" className="text-white">
+           Pedidos
+            </Link>
+          </a>
+          </ul>
+    
+
+        <button onClick={this.handleClick} className="btn btn-primary">Send</button>
+  </form>
+  </div>
     );
   }
 }
