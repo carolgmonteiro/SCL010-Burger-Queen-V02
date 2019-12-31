b5c1770b88dde14e0e1fe244b6f77045c726b31a
carolgmonteiro <carolgmonteiro@live.com>
2019-12-31 10:14:29 -0300

    Updates

---
+++ precache-manifest.942cca23cc05a25ab46b57a0b97b70a8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2294c558e3b8c2c8dc01ded18d646930",
+    "url": "/SCL010-Burger-Queen-V02/index.html"
+  },
+  {
+    "revision": "92f567163fc7628d2822",
+    "url": "/SCL010-Burger-Queen-V02/static/css/2.62261112.chunk.css"
+  },
+  {
+    "revision": "8c8114572285b0ec1785",
+    "url": "/SCL010-Burger-Queen-V02/static/css/main.c1e5c3fe.chunk.css"
+  },
+  {
+    "revision": "92f567163fc7628d2822",
+    "url": "/SCL010-Burger-Queen-V02/static/js/2.e59d214b.chunk.js"
+  },
+  {
+    "revision": "8c8114572285b0ec1785",
+    "url": "/SCL010-Burger-Queen-V02/static/js/main.80a57460.chunk.js"
+  },
+  {
+    "revision": "85c6dc85aba5728a3794",
+    "url": "/SCL010-Burger-Queen-V02/static/js/runtime~main.4186377a.js"
+  },
+  {
+    "revision": "6d2748a82ed487bf6855697551f267e4",
+    "url": "/SCL010-Burger-Queen-V02/static/media/LOGO BURGER QUEEN-04.6d2748a8.svg"
+  },
+  {
+    "revision": "fceaeb2cae67d42b1e6e0df2989534ad",
+    "url": "/SCL010-Burger-Queen-V02/static/media/delete_button.fceaeb2c.svg"
+  },
+  {
+    "revision": "46bdada06b000934e88ffa1a63fd6df2",
+    "url": "/SCL010-Burger-Queen-V02/static/media/icon-kitchen-yellow.46bdada0.svg"
+  },
+  {
+    "revision": "940e76fb6d1fdf59423c14551b0ec1d2",
+    "url": "/SCL010-Burger-Queen-V02/static/media/icon-menu-drinks.940e76fb.svg"
+  },
+  {
+    "revision": "62386ca434a376057becf399c43796bb",
+    "url": "/SCL010-Burger-Queen-V02/static/media/icon-menu-food.62386ca4.svg"
+  },
+  {
+    "revision": "d0c0c08afc31633ba885a6bf072e1cf8",
+    "url": "/SCL010-Burger-Queen-V02/static/media/icon-menu-yellow.d0c0c08a.svg"
+  },
+  {
+    "revision": "4ddd5a771dc0dad02f42e67df5166423",
+    "url": "/SCL010-Burger-Queen-V02/static/media/icon-resume-yellow.4ddd5a77.svg"
+  }
+]);
